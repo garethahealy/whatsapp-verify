d179b43414f2af2665b746e2e5fb3976ed4551c0
Gareth Healy <garethahealy@gmail.com>
2026-01-24 14:12:39 +0000

    added member test
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

---
+++ src/test/java/com/garethahealy/whatsappverify/model/MemberTest.java
@@ -0,0 +1,34 @@
+package com.garethahealy.whatsappverify.model;
+
+import com.google.i18n.phonenumbers.Phonenumber;
+import org.apache.directory.api.ldap.model.filter.FilterEncoder;
+import org.apache.directory.ldap.client.api.search.FilterBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MemberTest {
+
+    @Test
+    void fromBuildsMember() {
+        Phonenumber.PhoneNumber phoneNumber = new Phonenumber.PhoneNumber().setRawInput("+44 7818 511214");
+
+        Member member = Member.from(phoneNumber, "gahealy@redhat.com");
+
+        assertEquals(phoneNumber, member.whatsAppNumber());
+        assertEquals("gahealy@redhat.com", member.redHatEmailAddress());
+    }
+
+    @Test
+    void toArraySanitizesWhatsAppNumber() {
+        Phonenumber.PhoneNumber phoneNumber = new Phonenumber.PhoneNumber().setRawInput("+44 7818 511214");
+        Member member = new Member(phoneNumber, "gahealy@redhat.com");
+
+        List<String> result = member.toArray();
+
+        assertEquals(Arrays.asList("00447818511214", "gahealy@redhat.com"), result);
+    }
+}
